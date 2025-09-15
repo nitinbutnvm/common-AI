1ec18048ae9029f937e9ab06b368012269b2a7f8
nitinbutnvm <heynittinnn@gmail.com>
2025-09-15 21:36:01 +0530

    updated home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,20 @@
+import AiTools from "../components/AiTools"
+import Footer from "../components/Footer"
+import Hero from "../components/Hero"
 import Navbar from "../components/Navbar"
+import Plan from "../components/plan"
+import Testimonial from "../components/Testimonial"
 
 const Home = () => {
   return (
     <>
     
     <Navbar/>
+    <Hero/>
+    <AiTools/>
+    <Testimonial/>
+    <Plan/>
+    <Footer/>
     </>
   )
 }
