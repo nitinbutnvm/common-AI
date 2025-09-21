41ab6b76b7cff7686372a2437ba2fbbae47fe9f9
nitinbutnvm <heynittinnn@gmail.com>
2025-09-21 21:23:56 +0530

    feat: Updated routes

--- backend/routes/aiRoutes.js
+++ backend/routes/aiRoutes.js
@@ -9,6 +9,6 @@ const aiRouter =  express.Router()
 aiRouter.post('/generate-article', auth, generateArticle)
 aiRouter.post('/generate-blog-title', auth, generateBlogTitle)
 aiRouter.post('/generate-image', auth, generateImage)
-aiRouter.post('/remove-image-background',auth, upload.single("image"), removeImageBackground)
+aiRouter.post('/remove-image-background', upload.single("image"), auth, removeImageBackground)
 
 export default aiRouter
