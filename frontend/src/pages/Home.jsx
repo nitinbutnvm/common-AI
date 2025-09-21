06cd69558eca5086647eecff9d1a8446a5e3a39c
nitinbutnvm <heynittinnn@gmail.com>
2025-09-21 22:21:37 +0530

    Updated home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import AiTools from "../components/AiTools"
 import Footer from "../components/Footer"
 import Hero from "../components/Hero"
 import Navbar from "../components/Navbar"
-import Plan from "../components/plan"
+import Plan from "../components/Plan"
 import Testimonial from "../components/Testimonial"
 
 const Home = () => {
